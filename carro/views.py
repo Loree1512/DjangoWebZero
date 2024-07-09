3173f8ae303aa66a3d3eb2cc40ce124e8dde5097
Loree1512 <loreto.bravo@outlook.com>
2024-07-09 17:24:45 -0400

    views carrito

--- carro/views.py
+++ carro/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .carro import Carro
+from catalogo.models import Articulo
+from django.shortcuts import redirect
 
 # Create your views here.
+
+def agregar_articulo(request,articulo_id):
+    carro=Carro(request)
+    articulo=Articulo.objects.get(id=articulo_id)
+    carro.agregar(articulo=articulo)
+    return redirect ("catalogo")
+
+def eliminar_articulo(request,articulo_id):
+    carro=Carro(request)
+    articulo=Articulo.objects.get(id=articulo_id)
+    carro.eliminar(articulo=articulo)
+    return redirect ("catalogo")
+
+def restar_articulo(request,articulo_id):
+    carro=Carro(request)
+    articulo=Articulo.objects.get(id=articulo_id)
+    carro.restar_articulo(articulo=articulo)
+    return redirect ("catalogo")
+
+def limpiar_carro(request, articulo_id):
+    carro = Carro(request)
+    carro.limpiar_carro()
+    return redirect ("catalogo")
