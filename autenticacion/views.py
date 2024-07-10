cc7d6995e648c3519bf394c02dc2e0ea9fe25239
Loree1512 <loreto.bravo@outlook.com>
2024-07-10 02:37:39 -0400

    implementacion inicio de sesion y registro

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # Create your views here.
 
 class VRegistro(View):
@@ -10,4 +12,35 @@ class VRegistro(View):
         return render(request,'registro/registro.html',{'form':form})
 
     def post(self,request):
-        pass
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            usiario=form.save()
+            login(request, usiario)
+            return redirect ('Home')
+        else:
+            for msg in form.error_messages:
+                messages.error(request, msg)
+            return render(request,'registro/registro.html',{'form':form})
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect ('Home')
+
+def logear(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre_usuario = form.cleaned_data.get('username')
+            contra = form.cleaned_data.get('password')
+            usuario = authenticate(username=nombre_usuario, password=contra)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect('Home')
+            else:
+                messages.error(request, 'Usuario o contraseña incorrectos')
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos')
+    form=AuthenticationForm()
+    return render(request,'login/login.html',{'form':form})
+
+    
