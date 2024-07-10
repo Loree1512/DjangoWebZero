b3bcee65f711fb8dc6900df4a8fcea29e7c3da07
Loree1512 <loreto.bravo@outlook.com>
2024-07-10 03:41:15 -0400

    solucion error carrito

--- webzeroapp/views.py
+++ webzeroapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+from carro.carro import Carro
+
 
 # Create your views here.
 
 def home(request):
+    carro=Carro(request)
 
     return render(request, "webzeroapp/home.html")
 
